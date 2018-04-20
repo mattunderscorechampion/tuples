d4de6ffcfd1764cbc9896466b57446c5ae841805
Matt Champion <mattunderscorechampion@gmail.com>
2018-04-20 14:08:08 +0100

    Add container implementation.

---
+++ src/main/java/com/mattunderscore/tuples/Container.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright © 2018 Matt Champion
+ */
+
+package com.mattunderscore.tuples;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * A 1-tuple.
+ *
+ * @author Matt Champion 20/04/2018
+ */
+public final class Container<T0> implements V0<T0> {
+    private final T0 v0;
+
+    /*package*/ Container(T0 v0) {
+        this.v0 = requireNonNull(v0);
+    }
+
+    @Override
+    public T0 v0() {
+        return v0;
+    }
+
+    @Override
+    public void acceptV0(Consumer<? super T0> consumer) {
+        consumer.accept(v0);
+    }
+
+    @Override
+    public <U> U applyV0(Function<? super T0, U> function) {
+        return function.apply(v0);
+    }
+
+    /**
+     * Map value 0 to new container.
+     *
+     * @param function function to apply to value 0
+     * @param <U> the new type of value 0
+     * @return the new container
+     */
+    public <U> Container<U> mapV0(Function<? super T0, U> function) {
+        return map(function);
+    }
+
+    /**
+     * Map value 0 to new container.
+     *
+     * @param function0 function0 to apply to value 0
+     * @param <U0> the new type of value 0
+     * @return the new container
+     */
+    public <U0> Container<U0> map(Function<? super T0, U0> function0) {
+        return new Container<>(function0.apply(v0));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final Container<?> pair = (Container<?>) o;
+        return v0.equals(pair.v0);
+    }
+
+    @Override
+    public int hashCode() {
+        return v0.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + v0 + "]";
+    }
+
+    /**
+     * @return a new container
+     */
+    public static <T0, U0 extends T0> Container<T0> of(U0 v0) {
+        return new Container<>(v0);
+    }
+}
---
+++ src/test/java/com/mattunderscore/tuples/ContainerTest.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright © 2018 Matt Champion
+ */
+
+package com.mattunderscore.tuples;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/**
+ * Unit test for {@link Container}.
+ *
+ * @author Matt Champion 20/04/2018
+ */
+public final class ContainerTest {
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private Consumer<Object> consumer;
+    @Mock
+    private Function<Object, Object> function;
+
+    @Test
+    public void testAccess() {
+        final Container<Object> container = Container.of("a");
+
+        assertEquals("a", container.v0());
+    }
+
+    @Test
+    public void testAccept() {
+        final Container<Object> container = Container.of("a");
+
+        container.acceptV0(consumer);
+        verify(consumer).accept("a");
+    }
+
+    @Test
+    public void testApply() {
+        final Container<Object> container = Container.of("a");
+
+        container.applyV0(function);
+        verify(function).apply("a");
+    }
+
+    @Test
+    public void testMapV0() {
+        final Container<Object> container0 = Container.of("a");
+        final Container<Object> container1 = container0.mapV0(Function.identity());
+
+        assertEquals(container0, container1);
+    }
+
+    @Test
+    public void testMap() {
+        final Container<Object> container0 = Container.of("a");
+        final Container<Object> container1 = container0.map(Function.identity());
+
+        assertEquals(container0, container1);
+    }
+
+    @Test
+    public void testToString() {
+        final Container<Object> container = Container.of("a");
+
+        assertEquals("[a]", container.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        final Container<Object> container0 = Container.of("a");
+        final Pair<Object, String> container1 = Pair.of("a", "b");
+
+        assertTrue(container0.equals(container1));
+        assertTrue(container1.equals(container0));
+        assertEquals(container0.hashCode(), container1.hashCode());
+    }
+
+    @Test
+    public void testNotEqualsNull() {
+        final Container<Object> container0 = Container.of("a");
+
+        assertFalse(container0.equals(null));
+    }
+
+    @Test
+    public void testNotEqualsObject() {
+        final Container<Object> container0 = Container.of("a");
+
+        assertFalse(container0.equals(new Object()));
+    }
+
+    @Test
+    public void testNotEqualsDifferent() {
+        final Container<Object> container0 = Container.of("a");
+        final Container<Object> container1 = Container.of("b");
+
+        assertFalse(container0.equals(container1));
+    }
+
+    @Test
+    public void testEqualsSelf() {
+        final Container<Object> container0 = Container.of("a");
+
+        assertTrue(container0.equals(container0));
+    }
+}
