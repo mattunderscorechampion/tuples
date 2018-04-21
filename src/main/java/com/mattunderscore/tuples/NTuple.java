8c4a6fb793db44c25da3e6564ed5e48db7d955c8
Matt Champion <mattunderscorechampion@gmail.com>
2018-04-21 14:39:41 +0100

    Add an n-tuple implementation.

---
+++ src/main/java/com/mattunderscore/tuples/NTuple.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright © 2018 Matt Champion
+ */
+
+package com.mattunderscore.tuples;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * A n-tuple.
+ * <p>
+ * Lacks type and arity safety.
+ *
+ * @author Matt Champion 21/04/2018
+ */
+public final class NTuple {
+    private final Object[] values;
+
+    /*package*/ NTuple(Object[] values) {
+        this.values = requireNonNull(values);
+    }
+
+    /**
+     * Return value {@code i}.
+     *
+     * @param i the index of the value to access
+     * @param <T> the type of value {@code i}
+     * @return value {@code i}
+     * @throws ClassCastException if the expected type is not the actual type
+     * @throws IndexOutOfBoundsException if the arity is wrong
+     */
+    public <T> T v(int i) {
+        return (T) values[i];
+    }
+
+    /**
+     * Pass value {@code i} to {@code consumer}.
+     *
+     * @param i the index of the value to access
+     * @param <T> the type of value {@code i}
+     * @throws ClassCastException if the expected type is not the actual type
+     * @throws IndexOutOfBoundsException if the arity is wrong
+     */
+    public <T> void acceptV(int i, Consumer<? super T> consumer) {
+        consumer.accept(v(i));
+    }
+
+    /**
+     * Apply {@code function} to value {@code i} and return the result.
+     *
+     * @param <T> the type of value {@code i}
+     * @param <U> the new type of value {@code i}
+     * @throws ClassCastException if the expected type is not the actual type
+     * @throws IndexOutOfBoundsException if the arity is wrong
+     */
+    public <T, U> U applyV(int i, Function<? super T, U> function) {
+        return function.apply(v(i));
+    }
+
+    /**
+     * Map value {@code i} to new tuple.
+     *
+     * @param function function to apply to value {@code i}
+     * @param <T> the type of value {@code i}
+     * @param <U> the new type of value {@code i}
+     * @return the new tuple
+     * @throws ClassCastException if the expected type is not the actual type
+     * @throws IndexOutOfBoundsException if the arity is wrong
+     */
+    public <T, U> NTuple mapV(int i, Function<? super T, U> function) {
+        final T value = v(i);
+        final Object[] newValues = Arrays.copyOf(values, values.length);
+        newValues[i] = function.apply(value);
+        return new NTuple(newValues);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final NTuple nTuple = (NTuple) o;
+        return Arrays.equals(values, nTuple.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(values);
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(values);
+    }
+
+    /**
+     * @return a new n-tuple
+     */
+    public static NTuple of(Object... values) {
+        return new NTuple(Arrays.copyOf(values, values.length));
+    }
+}
---
+++ src/test/java/com/mattunderscore/tuples/NTupleTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright © 2018 Matt Champion
+ */
+
+package com.mattunderscore.tuples;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+/**
+ * Unit test for {@link NTuple}.
+ *
+ * @author Matt Champion 21/04/2018
+ */
+public final class NTupleTest {
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+
+    @Mock
+    private Consumer<Object> consumer;
+    @Mock
+    private Function<Object, Object> function;
+
+    @Test
+    public void testAccess() {
+        final NTuple tuple = NTuple.of("a", "b");
+
+        assertEquals("a", tuple.v(0));
+        assertEquals("b", tuple.v(1));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testAccessWrongArity() {
+        final NTuple tuple = NTuple.of("a", "b");
+
+        tuple.v(3);
+    }
+
+    @Test(expected = ClassCastException.class)
+    public void testAccessWrongType() {
+        final NTuple tuple = NTuple.of("a", "b");
+
+        final Integer integer = tuple.v(0);
+    }
+
+    @Test
+    public void testAccept() {
+        final NTuple tuple = NTuple.of("a", "b");
+
+        tuple.acceptV(0, consumer);
+        verify(consumer).accept("a");
+
+        tuple.acceptV(1, consumer);
+        verify(consumer).accept("b");
+    }
+
+    @Test
+    public void testApply() {
+        final NTuple tuple = NTuple.of("a", "b");
+
+        tuple.applyV(0, function);
+        verify(function).apply("a");
+
+        tuple.applyV(1, function);
+        verify(function).apply("b");
+    }
+
+    @Test
+    public void testMapV0() {
+        final NTuple tuple0 = NTuple.of("a", "b");
+        final NTuple tuple1 = tuple0.mapV(0, Function.identity());
+
+        assertEquals(tuple0, tuple1);
+    }
+
+    @Test
+    public void testMapV1() {
+        final NTuple tuple0 = NTuple.of("a", "b");
+        final NTuple tuple1 = tuple0.mapV(1, Function.identity());
+
+        assertEquals(tuple0, tuple1);
+    }
+
+    @Test
+    public void testToString() {
+        final NTuple tuple = NTuple.of("a", "b");
+
+        assertEquals("[a, b]", tuple.toString());
+    }
+
+    @Test
+    public void testEquals() {
+        final NTuple tuple0 = NTuple.of("a", "b");
+        final NTuple tuple1 = NTuple.of("a", "b");
+
+        assertTrue(tuple0.equals(tuple1));
+        assertTrue(tuple1.equals(tuple0));
+        assertEquals(tuple0.hashCode(), tuple1.hashCode());
+    }
+
+    @Test
+    public void testNotEqualsNull() {
+        final NTuple tuple0 = NTuple.of("a", "b");
+
+        assertFalse(tuple0.equals(null));
+    }
+
+    @Test
+    public void testNotEqualsObject() {
+        final NTuple tuple0 = NTuple.of("a", "b");
+
+        assertFalse(tuple0.equals(new Object()));
+    }
+
+    @Test
+    public void testNotEqualsDifferent() {
+        final NTuple tuple0 = NTuple.of("a", "b");
+        final NTuple tuple1 = NTuple.of("a", "c");
+
+        assertFalse(tuple0.equals(tuple1));
+    }
+
+    @Test
+    public void testEqualsSelf() {
+        final NTuple tuple1 = NTuple.of("a", "b");
+
+        assertTrue(tuple1.equals(tuple1));
+    }
+}
